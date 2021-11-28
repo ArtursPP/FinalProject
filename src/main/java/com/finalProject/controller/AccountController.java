e4cfe4f75508c4cec0e2d313dc1b183a61480a66
Arturs Pesikas-Petersons <artursp@yahoo.com>
2021-11-28 13:08:20 +0100

    Updated final project

--- src/main/java/com/finalProject/controller/AccountController.java
+++ src/main/java/com/finalProject/controller/AccountController.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/api/rest/Account.svc")
+@CrossOrigin
 public class AccountController {
 
 
--- src/test/java/com/finalProject/controller/AccountControllerIntegrationTest.java
+++ src/test/java/com/finalProject/controller/AccountControllerIntegrationTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
+
+import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +26,7 @@ class AccountControllerIntegrationTest {
     @Autowired
     private RestTemplate restTemplate;
 
+
     @Test
     void getAccountById() {
         Account account = new Account();
@@ -36,11 +40,31 @@ class AccountControllerIntegrationTest {
         assertEquals("EUR", accountDTO.getCurrency());
         assertEquals("ACTIVE", accountDTO.getStatus());
         assertEquals(2554.00, accountDTO.getBallance());
+
         accountRepository.delete(account);
 
     }
 
     @Test
+    @Transactional
     void saveNewAccount() {
+        AccountDTO accountDTO = new AccountDTO();
+        accountDTO.setAccountNumber("LV007UNLA666666669879");
+        accountDTO.setBallance(2001.00);
+        accountDTO.setCurrency("EUR");
+        accountDTO.setStatus("ACTIVE");
+
+        HttpEntity<AccountDTO> request = new HttpEntity<>(accountDTO);
+
+        AccountDTO savedAccountDTO = this.restTemplate.postForObject("http://localhost:" + port + "/api/rest/Account.svc/account", request, AccountDTO.class);
+        Long savedAccountDTOId = savedAccountDTO.getId();
+        Account accountFromDB = accountRepository.getById(savedAccountDTOId);
+        assertEquals("LV007UNLA666666669879", accountFromDB.getAccountNumber());
+        assertEquals(2001.00, accountFromDB.getBallance());
+        assertEquals("EUR", accountFromDB.getCurrency());
+        assertEquals(1, accountFromDB.getStatus());
+
+        accountRepository.delete(accountFromDB);
+
     }
 }
